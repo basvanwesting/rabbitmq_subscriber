de159824b04708115a7909622f77e28bd209cf8b
basvanwesting <basvanwesting@hotmail.com>
2020-09-23 13:38:59 +0200

    Add lager to extra_applications

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule RabbitmqSubscriber.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:lager, :logger],
       mod: {RabbitmqSubscriber.Application, []}
     ]
   end
